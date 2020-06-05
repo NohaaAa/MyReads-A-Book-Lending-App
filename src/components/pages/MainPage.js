d95a870e804a51ec673a6a0ee43ff72b49c446e6
noha-mohammed <nm1488@fayoum.edu.eg>
2020-06-05 23:43:56 +0200

    handling BooksAPI

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import * as BooksAPI from '../../BooksAPI';
+
 import Shelf from '../Shelf';
 
 class MainPage extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
+  }
+
+
+    
+    async componentDidMount() {
+
+      try {
+        const books = await BooksAPI.getAll();
+        console.log(books);
+        this.setState( { books } );
+      } catch(e) {}
+
+    }
+
     
     render() {
-        return (
-            <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                  <Shelf />
-                
-              </div>
-            </div>
-            <div className="open-search">
-              <Link className='btn' to='/search'>Add a book</Link>
+      // const { books } = this.state
+      if(!this.state.books.length) {
+        return 'loading!'
+      }
+      return (
+           
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+            <Shelf name='Currently Reading' books={this.state.books.filter(book => book.Shelf === "currentlyReading")}/>
+                <Shelf name='Want To Read' books={this.state.books.filter(book => book.Shelf === "wantToRead")}/>
+                <Shelf name='Read' books={this.state.books.filter(book => book.Shelf === "read")}/>
+             
+              
             </div>
           </div>
+          <div className="open-search">
+            <Link className='btn' to='/search'>Add a book</Link>
+          </div>
+        </div>
+
         )
     }
 }
